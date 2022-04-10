f7c713b7ed98bb5e08de6fea58f1437791d6e4fa
bjKim2 <rlaxodjs3@naver.com>
2022-04-11 02:28:12 +0900

    home

--- ELAPID_jsp_project/src/main/java/com/elapid/dto/OrderHistoryDto.java
+++ ELAPID_jsp_project/src/main/java/com/elapid/dto/OrderHistoryDto.java
@@ -1,5 +1,28 @@
 package com.elapid.dto;
 
+import java.sql.Timestamp;
+
 public class OrderHistoryDto {
 
+	private int uo_id;
+	private int uod_id;
+	private int p_id;
+	private String u_id;
+	
+	private String uo_name;
+	private String uo_tel;
+	private int uo_point;
+	private int uo_amount;
+	private int uo_discountedamount;
+	private Timestamp uo_date;
+	private String uo_coupon;
+	private String uo_specificaddress;
+	private String uo_address;
+	private int uo_shippingfee;
+	private String uo_paymentmethod;
+	private int uo_deliverycharge;
+	
+	
+	
+	
 }
